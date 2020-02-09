e68e324ec6dd21ad1118ea408fc1d4a8af4cf20c
Hartmut Bauer <hb42@gmx.de>
2020-02-09 20:00:21 +0100

    Build-Logik auf lokale tgz umstellen

--- lib/const/const.d.ts
+++
@@ -1,5 +0,0 @@
-export declare const authURL = "/authenticate";
-export declare const loginURL = "/login";
-export declare const staticURL = "/app";
-export declare const JwtHeader = "X-Auth-Token";
-export declare const JwtToken = "jwt-token";
--- lib/util.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const dateString: (millis: number, sec?: boolean | undefined, milli?: boolean | undefined) => string;
-export declare const makeSseUrl: (addr: string) => string;
