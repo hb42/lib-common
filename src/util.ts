e61ecd014960ac948d8b638b99ec21e718886577
Hartmut Bauer <hb42@gmx.de>
2020-03-24 22:57:02 +0100

    reformat using prettier

--- src/util.ts
+++ src/util.ts
@@ -10,8 +10,16 @@
  */
 export const dateString = (millis: number, sec?: boolean, milli?: boolean): string => {
   const d = new Date(millis);
-  let str = ("0" + d.getDate()).slice(-2) + "." + ("0" + (d.getMonth() + 1)).slice(-2) + "." +
-    d.getFullYear() + " " + ("0" + d.getHours()).slice(-2) + ":" + ("0" + d.getMinutes()).slice(-2);
+  let str =
+    ("0" + d.getDate()).slice(-2) +
+    "." +
+    ("0" + (d.getMonth() + 1)).slice(-2) +
+    "." +
+    d.getFullYear() +
+    " " +
+    ("0" + d.getHours()).slice(-2) +
+    ":" +
+    ("0" + d.getMinutes()).slice(-2);
   if (sec || milli) {
     str += ":" + ("0" + d.getSeconds()).slice(-2);
   }
